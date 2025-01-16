27a3093dace4535adf51de1dd35866ef5c41cf83
Utkarsh <141557962+UtkarshBhardwaj1@users.noreply.github.com>
2025-01-16 16:38:46 +0530

    Update main.java

--- 488_Find_all_numbers_disappeared_in_an_array/main.java
+++ 488_Find_all_numbers_disappeared_in_an_array/main.java
@@ -1,3 +1,4 @@
+//Using hash set
 class Solution {
     public List<Integer> findDisappearedNumbers(int[] nums) {
         Set<Integer> set=new HashSet<>(); 
@@ -15,4 +16,42 @@ class Solution {
         } 
         return result;
     }
+}
+
+
+//Using cyclic sort
+class Solution {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+       cSort(nums);
+
+       List<Integer>list=new ArrayList<Integer>();
+           
+    for(int i=0;i<nums.length;i++){
+        if(nums[i]!=i+1){
+            list.add(i+1);
+        }
+    } return list;      
+    }
+     
+     public void cSort(int nums[]){
+        int n=nums.length;
+        int ind=0;
+        while(ind<n){
+            int ele=nums[ind];
+            int corrPosition=ele-1;
+            if(nums[ind]!=nums[corrPosition]){
+                swap(nums,ind,corrPosition);
+
+            }else{
+                ind++;
+            }
+                    }
+     }
+
+     void swap(int input[],int index,int indexA){
+        int temp=input[index];
+        input[index]= input[indexA];
+        input[indexA]=temp;
+     }
+
 }
